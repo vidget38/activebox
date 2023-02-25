f5db952af84ba76c04b4064e0b4a280562a706e7
vidget38 <vidget38@mail.ru>
2023-02-25 09:53:38 +0300

    update: create js file and create fixed header

---
+++ js/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+$(function(){
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH;
+    let scrollPos = $(window).scrollTop();
+
+    $(window).on("scroll load resize", function() {
+        introH = intro.innerHeight()
+        scrollPos = $(this).scrollTop();
+
+        if(scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+    })
+
+});
